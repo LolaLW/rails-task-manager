8ba51ef1d9d0a20cae0d5fcc9ab4e997ded61903
Lolita Lugand <lyanlola77@gmail.com>
2022-08-16 15:50:34 +0200

    exo rails t m

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,29 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(tasks_params)
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
