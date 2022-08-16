294795dfc3dbbaad3c382ceecae75bd7ae72555f
Lolita Lugand <lyanlola77@gmail.com>
2022-08-16 16:42:50 +0200

    exo rails task manager s

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/tasks', to: 'tasks#index'
-  get '/task/:id', to: 'tasks#show', as: :task
+  # get '/tasks', to: 'tasks#index'
+  # get '/task/:id', to: 'tasks#show', as: :task
 
-  get '/tasks/new', to: 'tasks#new' # -> afficher le formulaire
-  post '/tasks', to: 'tasks#create'
+  # get '/tasks/new', to: 'tasks#new' # -> afficher le formulaire
+  # post '/tasks', to: 'tasks#create'
 
-  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'task/:id', to: 'tasks#update'
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'task/:id', to: 'tasks#update'
 
-  delete 'task/:id', to: 'tasks#destroy'
+  # delete 'task/:id', to: 'tasks#destroy'
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :tasks, only: [:index, :show]
 end
